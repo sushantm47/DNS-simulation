7b065ee6d402f5eb895e25ea8197da69c7a18a70
Sushant <vettel@pop-os.localdomain>
2020-04-29 23:23:15 +0530

    added loader

--- dnsserver.py
+++ dnsserver.py
@@ -1,6 +1,9 @@
 import socket
 import json
 import sys
+import csv
+from progressbar import ProgressBar
+pbar = ProgressBar()
 
 
 def dns(input):
@@ -23,21 +26,59 @@ def dns(input):
     file1.close()
     return(addr)
 
-s = socket.socket()
-port = int(sys.argv[1])
-print("Socket created")
-s.bind(('',port))
-print("socket binded to port",port)
-s.listen(5)
-print("Socket is listening")
+def startserver():
+    s = socket.socket()
+    port = int(sys.argv[1])
+    print("Socket created")
+    s.bind(('',port))
+    print("socket binded to port",port)
+    s.listen(5)
+    print("Socket is listening")
 
-while True:
-    c,addr = s.accept()
-    print("Connection received")
-    data = c.recv(1024).decode()
-    print(data)
-    result=str(dns(data))
-    print(result)
-    c.send(bytes("".join(result),"utf-8"))
-    c.close()
-exit()
+    while True:
+        c,addr = s.accept()
+        print("Connection received")
+        data = c.recv(1024).decode()
+        print(data)
+        result=str(dns(data))
+        print(result)
+        c.send(bytes("".join(result),"utf-8"))
+        c.close()
+    exit()
+
+def refreshserverdata():
+    newdata = {} 
+    l=[]
+    with open('top500Domains.csv', 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        for row in reader:
+            name="".join(row[1])
+            l.append(name)
+        # print(newdata)
+    error=0
+    for remote_host in pbar(l):
+            remote_host = remote_host.strip() # \n (new line) at the end of the line would cause error even when host exists
+            try:
+                addr=socket.gethostbyname(remote_host)
+                # print(f"And IP address is ",addr)
+                newdata[remote_host] = addr
+            except socket.gaierror as se:
+                error+=1
+    print("Server successfully refreshed")
+    file2 = open("data.json","w")
+    file2.write(json.dumps(newdata))
+
+# refreshserverdata()
+flag=1
+print("DNS SERVER"," 1  -> Start server"," 2  -> Refresh server data","-1  -> Quit",sep="\n")
+while flag:
+    t=int(input())
+    if(t==1):
+        startserver()
+    elif(t==2):
+        refreshserverdata()
+    elif(t==-1):
+        print("Shutting down the server..","Successfully shutdown",sep="\n")
+        flag=0
+    else:
+        print("please enter valid choice")
--- playground.py
+++ playground.py
@@ -2,10 +2,10 @@ import csv
 import socket
 import json
 
-file1 = open("data.json","r")
-localdata = json.loads(file1.read()) 
+file2 = open("data.json","r")
+newdata = json.loads(file2.read()) 
 
-file1.close
+file2.close
 
 l=[]
 with open('top500Domains.csv', 'r') as csv_file:
@@ -13,7 +13,7 @@ with open('top500Domains.csv', 'r') as csv_file:
     for row in reader:
         name="".join(row[1])
         l.append(name)
-    print(localdata)
+    print(newdata)
 
 for remote_host in l:
             remote_host = remote_host.strip() # \n (new line) at the end of the line would cause error even when host exists
@@ -21,9 +21,9 @@ for remote_host in l:
             try:
                 addr=socket.gethostbyname(remote_host)
                 print(f"And IP address is ",addr)
-                localdata[remote_host] = addr
+                newdata[remote_host] = addr
             except socket.gaierror as se:
                 print(f"Not done: {se}") # this will catch error when socket.gethostbyname
 
-file1 = open("data.json","w")
-file1.write(json.dumps(localdata))
+file2 = open("data.json","w")
+file2.write(json.dumps(newdata))
